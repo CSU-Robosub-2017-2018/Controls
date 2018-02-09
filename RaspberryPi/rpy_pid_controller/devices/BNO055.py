5a9e5e00d505eeaf1e98f74f2289d11fb978d7f1
Nicholas Baron <nick.john.baron@gmail.com>
2018-02-08 20:24:00 -0700

    fix?

--- RaspberryPi/rpy_pid_controller/devices/BNO055.py
+++ RaspberryPi/rpy_pid_controller/devices/BNO055.py
@@ -261,7 +261,7 @@ class BNO055:
 
     def getVector(self, vectorType):
         buf = self.readBytes(vectorType, 6)
-        xyz = (struct.unpack('hhh', ''.join(struct.pack('BBBBBB', buf[0], buf[1], buf[2], buf[3], buf[4], buf[5]))))
+        xyz = struct.unpack('hhh', struct.pack('BBBBBB', buf[0], buf[1], buf[2], buf[3], buf[4], buf[5]))
         if vectorType == BNO055.VECTOR_MAGNETOMETER:
             scalingFactor = 16.0
         elif vectorType == BNO055.VECTOR_GYROSCOPE:
@@ -276,8 +276,7 @@ class BNO055:
 
     def getQuat(self):
         buf = self.readBytes(BNO055.BNO055_QUATERNION_DATA_W_LSB_ADDR, 8)
-        wxyz = (struct.unpack('hhhh', ''.join(
-            struct.pack('BBBBBBBB', buf[0], buf[1], buf[2], buf[3], buf[4], buf[5], buf[6], buf[7]))))
+        wxyz = (struct.unpack('hhhh', struct.pack('BBBBBBBB', buf[0], buf[1], buf[2], buf[3], buf[4], buf[5], buf[6], buf[7])))
         return tuple([i * (1.0 / (1 << 14)) for i in wxyz])
 
     def readBytes(self, register, numBytes=1):
@@ -290,8 +289,7 @@ class BNO055:
 if __name__ == '__main__':
     bno = BNO055()
     if bno.begin() is not True:
-        print
-        "Error initializing device"
+        print("Error initializing device")
         exit()
     time.sleep(1)
     bno.setExternalCrystalUse(True)
