ae797b4c99a4aa3b954fa87ceb43939c1fc79b05
Nicholas Baron <nick.john.baron@gmail.com>
2018-02-26 16:23:01 -0700

    added steting main for arduino class

--- RaspberryPi/rpy_arduino_motorcontroller/MotorController_string.py
+++ RaspberryPi/rpy_arduino_motorcontroller/MotorController_string.py
@@ -6,6 +6,8 @@ Description: This class writes info over USB to an arduino to control pwm for ES
 
 import time
 import threading
+import serial
+from RaspberryPi.helpers import helpers
 
 class MotorController:
 
@@ -14,12 +16,15 @@ class MotorController:
     pwm_max = 2000
     pwm_min = 1000
 
-    def __init__(self, serialDevice):
+    def __init__(self, serialDevice, Debug = False):
+        self.Debug = Debug
         self.serialDevice = serialDevice
         self.data = {6, 1500, 1500, 1500, 1500, 1500, 1500}
 
     def arm(self):
         if not self.armed:
+            if self.Debug:
+                print("Armed")
             self.data = {5, 1500, 1500, 1500, 1500, 1500, 1500}
             self.serialDevice.write()
             self.armed = True
@@ -29,12 +34,17 @@ class MotorController:
 
     def disarm(self):
         if self.armed:
+            if self.Debug:
+                print("Disrmed")
             self.data = {6, 1500, 1500, 1500, 1500, 1500, 1500}
             self.serialDevice.write()
             self.armed = False
 
     def update(self):
         while self.armed:
+            if self.Debug:
+                print("Update")
+                print(str(self.data))
             self.write()
             time.sleep(.25)
 
@@ -42,9 +52,14 @@ class MotorController:
         if self.armed:
             self.data[2 * axis + 1] = speed0
             self.data[2 * axis + 2] = speed1
+            if self.Debug:
+                print("Set_Speed")
+                print(str(axis) + " " + str(speed0) + " " + str(speed1))
 
     def write(self):
         try:
+            if self.Debug:
+                print("Write")
             s = str(self.data[0])
             for i in range(1, len(self.data)):
                 s += "," + str(self.data)
@@ -53,4 +68,28 @@ class MotorController:
                 print("[ERROR] values may not have written properly!")
         except:
             print("[ERROR] serial write failed!")
+
+    def clear(self):
+        for i in range(0,3):
+            self.set_speed(i, self.pwm_norm, self.pwm_norm)
+
+
+serial = serial.Serial(helpers.find_arduino())
+motors = MotorController(serial, Debug=True)
+print("Motors Started")
+motors.arm()
+print("Motors Armed")
+
+try:
+    while True:
+        motors.set_speed(0,1400,1600)
+        motors.set_speed(1,1600,1400)
+        motors.set_speed(2, 1400, 1600)
+        time.sleep(2)
+        motors.clear()
+        time.sleep(2)
+finally:
+    motors.disarm()
+    serial.close()
+
 
