c845297b993047bb896d9ed9f86e861c2a985b44
Nicholas Baron <nick.john.baron@gmail.com>
2018-02-26 15:58:55 -0700

    edit to use mpu 6050

--- Feb27Test.py
+++ Feb27Test.py
@@ -1,30 +1,32 @@
 from RaspberryPi.rpy_pid_controller.Feb27test_pid_shell import pid_wrapper
 from RaspberryPi.rpy_arduino_motorcontroller.MotorController_string import MotorController
-from RaspberryPi.rpy_pid_controller.devices.BNO055 import BNO055
+from RaspberryPi.rpy_pid_controller.devices.mpu6050 import mpu6050
 from RaspberryPi.helpers import helpers
 from serial import Serial
 from time import sleep
 
 
-imu = BNO055()
+imu = mpu6050(0x68)
 pid = pid_wrapper(imu)
 
 serial = Serial(helpers.find_arduino()[0])
 motors = MotorController(serial)
 
-targets = {imu.getVector(BNO055.VECTOR_EULER)} #FIXME Need Correct target angles and correct orientation yaw angle.
+targets = {0,0} #FIXME Need Correct target angles and correct orientation yaw angle.
 pid.set_targets(targets)
 pid.run()
 
 try:
     while not motors.armed():
-        if imu.getVector(BNO055.VECTOR_EULER)[0] == abs(360-targets[0]): #FIXME add ability to be within range.
+        if imu.get_accel_data()['x'] == abs(360-targets[0]): #FIXME add ability to be within range.
             motors.arm()
+            print("armed")
             sleep(10)
 
     while motors.armed():
-        if imu.getVector(BNO055.VECTOR_EULER)[0] == abs(360-targets[0]):
+        if imu.get_accel_data()['x'] == abs(360-targets[0]):
             motors.disarm()
+            print("disarmed")
         else:
             pidNum = pid.get_pid()
             for i in range(0, len(pidNum)):
@@ -33,6 +35,7 @@ try:
                 elif pidNum[i] < motors.pwm_min-motors.pwm_norm:
                     pidNum[i] = motors.pwm_min-motors.pwm_norm
                 motors.write(i,motors.pwm_norm+pidNum[i], motors.pwm_norm-pidNum[i])
+        sleep(0.25)
 
 finally:
     pid.stop()
--- RaspberryPi/rpy_pid_controller/Feb27test_pid_shell.py
+++ RaspberryPi/rpy_pid_controller/Feb27test_pid_shell.py
@@ -1,33 +1,23 @@
-from RaspberryPi.rpy_pid_controller.pid.pid_controller_bno055 import pid_controller
-from RaspberryPi.rpy_pid_controller.devices.BNO055 import BNO055
+from RaspberryPi.rpy_pid_controller.pid.pid_controller_6050 import pid_controller
 import time
 
 class pid_wrapper:
 
-    def __init__(self, imu):
-        if imu.begin() is not True:
-            print("Error initializing device")
-            exit()
-        time.sleep(1)
-
-        self.pid_pitch = pid_controller(imu, 0, BNO055.VECTOR_EULER)
-        self.pid_roll = pid_controller(imu, 1, BNO055.VECTOR_EULER)
-        self.pid_yaw = pid_controller(imu, 2, BNO055.VECTOR_EULER)
+    def __init__(self, mpu):
+        self.pid_pitch = pid_controller(mpu, 'x', 'z')
+        self.pid_roll = pid_controller(mpu, 'y', 'z')
 
     def get_pid(self):
-        return {self.pid_pitch.get_pid(), self.pid_roll.get_pid(), self.pid_yaw.get_pid()}
+        return {self.pid_pitch.get_pid(), self.pid_roll.get_pid()}
 
     def set_targets(self, targets):
         self.pid_pitch.set_target_angle(targets[0])
         self.pid_roll.set_target_angle(targets[1])
-        self.pid_yaw.set_target_angle(targets[2])
 
     def run(self):
         self.pid_pitch.run()
         self.pid_roll.run()
-        self.pid_yaw.run()
 
     def stop(self):
         self.pid_pitch.stop()
         self.pid_roll.stop()
-        self.pid_yaw.stop()
