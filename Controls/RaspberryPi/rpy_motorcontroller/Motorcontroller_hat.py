f4b615dbf6ca61deae4a011770b7fea5946c7954
Nicholas Baron <njbaron@users.noreply.github.com>
2018-03-25 14:48:26 -0600

    now i understand better how this importing works
    with math fixes

--- Controls/RaspberryPi/rpy_motorcontroller/Motorcontroller_hat.py
+++ Controls/RaspberryPi/rpy_motorcontroller/Motorcontroller_hat.py
@@ -1,43 +1,48 @@
 import sys
 sys.path.append("/home/pi/Github/Adafruit_Python_PCA9685")
-from Adafruit_PCA9685 import PCA9685
+import Adafruit_PCA9685
 from time import sleep
 
 class MotorController:
 
-    def __init__(self):
-        self.pwm = PCA9685()
-        self.pwm.set_pwm_freq(50)
-        self.armed = False
+    armed = False
+    frequency = 60
 
-    def get_pulse(self, pulse):
+    def __init__(self, ):
+        self.pwm = Adafruit_PCA9685.PCA9685()
+        self.pwm.set_pwm_freq(self.frequency)
+        self.pulse_per_bit = self.get_ppb(self.frequency)
+
+    def get_ppb(self, freq):
+        self.pwm.set_pwm_freq(freq)
         pulse_length = 1000000  # 1,000,000 us per second
-        pulse_length //= 50  # 60 Hz
+        pulse_length //= freq
         print('{0}us per period'.format(pulse_length))
         pulse_length //= 4096  # 12 bits of resolution
         print('{0}us per bit'.format(pulse_length))
-        pulse *= 1000
-        pulse //= pulse_length
-        return pulse
+        return pulse_length
 
-    def set_servo_pulse(self, channel, pulse):
-        self.pwm.set_pwm(channel, 0, self.get_pulse(pulse))
+    def get_bit(self, microsecond):
+        return microsecond / self.pulse_per_bit
 
-    def set_all_servo_pulse(self, pulse):
-        self.pwm.set_all_pwm(0, self.get_pulse(pulse))
+    def set_microseconds(self, channel, microsecond):
+        self.pwm.set_pwm(channel, 0, self.get_bit(microsecond))
+
+    def set_all_microseconds(self, microsecond):
+        self.pwm.set_all_pwm(0, self.get_bit(microsecond))
 
     def arm(self):
         print("Arm")
-        self.set_all_servo_pulse(1500)
+        self.set_all_microseconds(1500)
         self.armed = True
 
     def disarm(self):
         print("Disarm")
-        self.set_all_servo_pulse(0)
+        self.set_all_microseconds(0)
         self.armed = False
 
-    def write(self, axis, pulse0, pulse1):
+    def write(self, axis, ms0, ms1):
         print("Write")
         if self.armed:
-            self.set_servo_pulse(2 * axis, pulse0)
-            self.set_servo_pulse(2 * axis + 1, pulse1)
+            self.set_servo_pulse(2 * axis, ms0)
+            self.set_servo_pulse(2 * axis + 1, ms1)
