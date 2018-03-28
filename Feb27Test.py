5a09897c07565e065099e41601eea3c6940c00f6
Nicholas Baron <njbaron@users.noreply.github.com>
2018-03-27 23:58:07 -0600

    added some things

--- Feb27Test.py
+++ Feb27Test.py
@@ -1,6 +1,5 @@
 import sys
 sys.path.append("/home/nick/Github/Controls/RaspberryPi/")
-print(str(sys.path))
 from RaspberryPi.rpy_pid_controller.Feb27test_pid_shell import pid_wrapper
 from RaspberryPi.rpy_motorcontroller.MotorController_hat import MotorController
 from RaspberryPi.rpy_pid_controller.devices.mpu6050 import mpu6050
@@ -17,27 +16,29 @@ pid.set_targets(targets)
 pid.run()
 
 try:
-    while not motors.armed:
-        print(str(imu.get_accel_data()))
-        if imu.get_accel_data()['z'] < -8: #FIXME add ability to be within range.
-            motors.arm()
-            print("armed")
-            sleep(10)
+    while True:
+        while not motors.armed:
+            print(str(imu.get_accel_data()))
+            if imu.get_accel_data()['z'] < -8: #FIXME add ability to be within range.
+                motors.arm()
+                print("armed")
+                sleep(10)
+            sleep(0.25)
 
-    while motors.armed:
-        print(str(imu.get_accel_data()))
-        if imu.get_accel_data()['z'] < -8:
-            motors.disarm()
-            print("disarmed")
-        else:
-            pidNum = pid.get_pid()
-            for i in range(0, len(pidNum)):
-                if pidNum[i] > motors.pwm_max-motors.pwm_norm:
-                    pidNum[i] = motors.pwm_max-motors.pwm_norm
-                elif pidNum[i] < motors.pwm_min-motors.pwm_norm:
-                    pidNum[i] = motors.pwm_min-motors.pwm_norm
-                motors.write(i, motors.pwm_norm+pidNum[i], motors.pwm_norm-pidNum[i])
-        sleep(0.25)
+        while motors.armed:
+            print(str(imu.get_accel_data()))
+            if imu.get_accel_data()['z'] < -8:
+                motors.disarm()
+                print("disarmed")
+            else:
+                pidNum = pid.get_pid()
+                for i in range(0, len(pidNum)):
+                    if pidNum[i] > motors.pwm_max-motors.pwm_norm:
+                        pidNum[i] = motors.pwm_max-motors.pwm_norm
+                    elif pidNum[i] < motors.pwm_min-motors.pwm_norm:
+                        pidNum[i] = motors.pwm_min-motors.pwm_norm
+                    motors.write(i, motors.pwm_norm+pidNum[i], motors.pwm_norm-pidNum[i])
+            sleep(0.25)
 
 finally:
     pid.stop()
